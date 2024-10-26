e4030496df0af94a9aa00a2e2f334f64a67a5b66
d4wns-l1ght <neovx.23@proton.me>
2024-10-26 14:15:36 +1300

    Actually fix java highlight problems

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -8,9 +8,30 @@ return {
 			require('onedark').setup {
 				style = 'darker',
 				term_colors = true,
+				colors = {
+
+				},
+				highlights = {
+					["@lsp.type.modifier.java"] = { fg = "$purple" },
+					["@type.builtin.java"] = { fg = "$purple" },
+					["@lsp.type.parameter.java"] = { fg = "$orange" },
+					["@variable.member.java"] = { fg = "$red" },
+					["@lsp.typemod.property.private.java"] = { fg = "$red" },
+
+
+					["@lsp.type.variable.java"] = { fg = "$none" },
+					["@lsp.type.property.java"] = { fg = "$none" },
+					["@lsp.mod.declaration.java"] = { fg = "$none" },
+				},
 			}
 			require('onedark').load()
 		end,
+	},
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {},
 	},
 	-- programming stuff
 	-- TODO
