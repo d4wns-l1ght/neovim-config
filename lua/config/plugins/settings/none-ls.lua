71ca7912bfa3df5d4d23d694ba0a86fecc74766b
d4wns-l1ght <neovx.23@proton.me>
2025-04-08 14:00:34 +1200

    Remove gitsigns codeaction from none-ls
    Conflicted with rustaceanvim + i already have keymaps. also stop lua-ls
    from formatting in favour of stylua

--- lua/config/plugins/settings/none-ls.lua
+++ lua/config/plugins/settings/none-ls.lua
@@ -3,7 +3,6 @@ local null_ls = require("null-ls")
 null_ls.setup({
   -- https://github.com/nvimtools/none-ls.nvim/blob/main/doc/BUILTINS.md
   sources = {
-    null_ls.builtins.code_actions.gitsigns,
     null_ls.builtins.diagnostics.dotenv_linter,
     null_ls.builtins.diagnostics.gitlint,
     null_ls.builtins.diagnostics.hadolint, -- dockerfiles
