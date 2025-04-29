e5d3cc123721ac2175e9323e6ebb3e9644195d90
d4wns-l1ght <neovx.23@proton.me>
2025-04-29 23:18:14 +1200

    Update whichkey opts

--- lua/config/plugins/qol/functional.lua
+++ lua/config/plugins/qol/functional.lua
@@ -2,7 +2,40 @@ return {
   { "LintaoAmons/cd-project.nvim", version = "*", cmd = "CdProject" },
   {
     "folke/which-key.nvim",
-    opts = {},
+    ---@class wk.Opts
+    opts = {
+      preset = "modern",
+      keys = {
+        Up = "<Up>",
+        Down = "<Down>",
+        Left = "<Left>",
+        Right = "<Right>",
+        C = "C-",
+        M = "M-",
+        D = "D-",
+        S = "S-",
+        CR = "<CR>",
+        Esc = "<Esc>",
+        ScrollWheelUp = "<ScrollWheelUp>",
+        ScrollWheelDown = "<ScrollWheelDown>",
+        NL = "<NL>",
+        BS = "<BS>",
+        Space = "<Space>",
+        Tab = "<Tab>",
+        F1 = "<F1>",
+        F2 = "<F2>",
+        F3 = "<F3>",
+        F4 = "<F4>",
+        F5 = "<F5>",
+        F6 = "<F6>",
+        F7 = "<F7>",
+        F8 = "<F8>",
+        F9 = "<F9>",
+        F10 = "<F10>",
+        F11 = "<F11>",
+        F12 = "<F12>",
+      },
+    },
     lazy = false, -- needed to set keymaps
     keys = {
       {
