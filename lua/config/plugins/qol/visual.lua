f426f0c38d412eebada54d35a43ad3a35160affe
d4wns-l1ght <neovx.23@proton.me>
2025-05-08 00:25:30 +1200

    Remove beacon.nvim

--- lua/config/plugins/qol/visual.lua
+++ lua/config/plugins/qol/visual.lua
@@ -63,7 +63,6 @@ return {
   -- Removes search highlights once done
   { "nvimdev/hlsearch.nvim", lazy = false, opts = {} },
   -- makes a little flash whenever the cursor moves
-  { "DanilaMihailov/beacon.nvim", event = "BufRead", opts = {} },
   { "HiPhish/rainbow-delimiters.nvim", event = "BufRead" },
   { "lukas-reineke/indent-blankline.nvim", event = "BufRead", main = "ibl", opts = {} },
   { "RRethy/vim-illuminate", event = "BufRead" },
