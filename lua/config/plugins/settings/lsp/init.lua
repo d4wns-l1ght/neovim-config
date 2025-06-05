d1748a46e05b2f2fbaf3c581ae8d8dd866d1cd76
d4wns-l1ght <neovx.23@proton.me>
2025-06-05 18:32:14 +1200

    Add HTML LSP (idk if its working though)

--- lua/config/plugins/settings/lsp/init.lua
+++ lua/config/plugins/settings/lsp/init.lua
@@ -1,7 +1,7 @@
 require("config.plugins.settings.lsp.autocmds")
 
-vim.lsp.enable('lua_ls')
-vim.lsp.config('lua_ls', {
+vim.lsp.enable("lua_ls")
+vim.lsp.config("lua_ls", {
   settings = {
     Lua = {
       diagnostics = {
@@ -9,6 +9,24 @@ vim.lsp.config('lua_ls', {
       },
       format = {
         enable = false,
+      },
+    },
+  },
+})
+vim.lsp.enable("marksman")
+vim.lsp.enable("html")
+vim.lsp.config("html", {
+  filetypes = {"html", "templ", "htmldjango"},
+  settings = {
+    html = {
+      format = {
+        templating = true,
+        wrapLineLength = 120,
+        wrapAttributes = "auto",
+      },
+      hover = {
+        documentation = true,
+        references = true,
       },
     },
   },
