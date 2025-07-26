ae507186586a0cacd66d7d14a6cf2649630c3b12
d4wns-l1ght <neovx.23@proton.me>
2025-07-26 22:42:41 +1200

    Add some themes
    Also swap out onedark for onedarkpro

--- lua/config/plugins/themes.lua
+++ lua/config/plugins/themes.lua
@@ -24,44 +24,22 @@ return {
       end,
     },
   },
+  -- Cool-toned
   {
     "EdenEast/nightfox.nvim",
     lazy = true,
-    opts = function()
-      return {
-        transparent = is_transparent or false,
-      }
-    end,
+    opts = {
+      transparent = is_transparent or false,
+    },
   },
-  {
-    "navarasu/onedark.nvim",
-    lazy = true,
-    opts = function()
-      return {
-        transparent = is_transparent or false,
-        style = "darker",
-        term_colors = true,
-        colors = {
-          light_red = "#d17277",
-          light_orange = "#e9c28a",
-          light_yellow = "#dfc184",
-          light_green = "#a0c180",
-          light_blue = "#73ade9",
-          light_purple = "#bb7bd7",
-          light_cyan = "6fb3bf",
-        },
-        highlights = {
-          ["@lsp.type.modifier.java"] = { fg = "$purple" },
-          ["@type.builtin.java"] = { fg = "$purple" },
-          ["@lsp.type.parameter.java"] = { fg = "$orange" },
-          ["@variable.member.java"] = { fg = "$red" },
-          ["@lsp.typemod.property.private.java"] = { fg = "$red" },
-
-          ["@lsp.type.variable.java"] = { fg = "$none" },
-          ["@lsp.type.property.java"] = { fg = "$none" },
-          ["@lsp.mod.declaration.java"] = { fg = "$none" },
-        },
-      }
-    end,
-  },
+  { "fynnfluegge/monet.nvim" },
+  { "folke/tokyonight.nvim" }, -- tokyonight-moon
+  { "ray-x/aurora" },
+  { "olimorris/onedarkpro.nvim" },
+  -- Warm-toned
+  { "rebelot/kanagawa.nvim" },
+  { "vague2k/vague.nvim" },
+  -- less liked/incomplete
+  { "mcauley-penney/techbase.nvim", lazy = true },
+  { "kaplanz/deku.nvim", lazy = true, dependencies = "rktjmp/lush.nvim" },
 }
